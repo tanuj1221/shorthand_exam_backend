83b1c556b0ebb8a214ac4baca4481c05ceb22671
tanuj1221 <tcchavan999@gmail.com>
2024-09-26 17:40:47 +0530

    sql remote conenection added

--- config/db1.js
+++ config/db1.js
@@ -3,6 +3,7 @@ require('dotenv').config(); // Load environment variables from .env file
 
 const connection = mysql.createPool({
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 3306, // Add this line
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
